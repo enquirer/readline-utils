453061a5b6d064c151e2fcba92b12eec37020d72
jonschlinkert <jon.schlinkert@sellside.com>
2017-05-09 14:19:00 -0400

    update tests

--- test/keypress.js
+++ test/keypress.js
@@ -9,8 +9,8 @@ function listen(val, num, cb) {
   var events = [];
   var count = 0;
 
-  process.stdin.on('keypress', function(str, key) {
-    events.push({val: str, key: key});
+  process.stdin.on('keypress', function(name, key) {
+    events.push({name: name, key: key});
     count++;
   });
 
@@ -28,7 +28,8 @@ describe('.keypress', function() {
 
   it('should normalize number events', function(cb) {
     listen(9, 1, function(events) {
-      assert.strictEqual(events[0].val, '9');
+      assert.strictEqual(events[0].name, 'number');
+      assert.strictEqual(events[0].key.value, 9);
       cb();
     });
   });
@@ -42,29 +43,31 @@ describe('.keypress', function() {
 
   it('should normalize newline events', function(cb) {
     listen('\n', 1, function(events) {
-      assert.strictEqual(events[0].val, '\n');
+      assert.strictEqual(events[0].name, 'enter');
       assert.strictEqual(events[0].key.name, 'enter');
+      assert.strictEqual(events[0].key.value, '\n');
       cb();
     });
   });
 
   it('should normalize carriage return events', function(cb) {
     listen('\r', 1, function(events) {
-      assert.strictEqual(events[0].val, '\r');
+      assert.strictEqual(events[0].name, 'return');
       assert.strictEqual(events[0].key.name, 'return');
+      assert.strictEqual(events[0].key.value, '\r');
       cb();
     });
   });
 
   it('should emit multiple events for multiple characters', function(cb) {
     listen('bar', 3, function(events) {
-      assert.strictEqual(events[0].val, 'b');
+      assert.strictEqual(events[0].name, 'b');
       assert.strictEqual(events[0].key.name, 'b');
 
-      assert.strictEqual(events[1].val, 'a');
+      assert.strictEqual(events[1].name, 'a');
       assert.strictEqual(events[1].key.name, 'a');
 
-      assert.strictEqual(events[2].val, 'r');
+      assert.strictEqual(events[2].name, 'r');
       assert.strictEqual(events[2].key.name, 'r');
       cb();
     });
@@ -74,19 +77,19 @@ describe('.keypress', function() {
     listen('@', 1, function(events) {
       assert.strictEqual(events[0].key.shift, true);
       assert.strictEqual(events[0].key.name, '@');
-      assert.strictEqual(events[0].val, '@');
+      assert.strictEqual(events[0].name, '@');
     });
 
     listen('!', 1, function(events) {
       assert.strictEqual(events[0].key.shift, true);
       assert.strictEqual(events[0].key.name, '!');
-      assert.strictEqual(events[0].val, '!');
+      assert.strictEqual(events[0].name, '!');
     });
 
     listen('*', 1, function(events) {
       assert.strictEqual(events[0].key.shift, true);
       assert.strictEqual(events[0].key.name, '*');
-      assert.strictEqual(events[0].val, '*');
+      assert.strictEqual(events[0].name, '*');
       cb();
     });
   });
